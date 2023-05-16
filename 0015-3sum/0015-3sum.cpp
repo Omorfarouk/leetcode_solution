43f8b06855f91314b4132e561fc32665cba5defd
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2023-05-16 11:42:15 +0600

    Time: 1502 ms (13.88%), Space: 191.6 MB (18.92%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+       int n=nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>output;
+        set<vector<int>>s;
+        
+        for(int i=0; i<n; i++){
+            int j=i+1;
+            int k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    s.insert({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+    
+            }
+        }
+        for(auto triplates : s){
+            output.push_back(triplates);
+        }
+        return output;
+      
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
