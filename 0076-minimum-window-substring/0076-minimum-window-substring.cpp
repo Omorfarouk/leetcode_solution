a2ce1898dd1c38554f54437f0b3efb0c3bd4fe63
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2023-08-10 16:23:43 +0600

    Time: 27 ms (54.14%), Space: 8 MB (36.03%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,64 +1,35 @@
 
-
-
-
-
-
-
-
+//ata final solution
 
 class Solution {
 public:
-    std::string minWindow(std::string s, std::string t) {
-        if (t.empty()) {
-            return "";
-        }
-
-        std::unordered_map<char, int> countT, window;
-        for (char c : t) {
-            countT[c] = 1 + countT[c];
-        }
-
-        int have = 0, need = countT.size();
-        int resStart = -1, resEnd = -1;
-      //  int resLen = std::numeric_limits<int>::max();
-     int   resLen=INT_MAX;
-        int l = 0;
-
-        for (int r = 0; r < s.length(); ++r) {
-            char c = s[r];
-            window[c] = 1 + window[c];
-
-            if (countT.find(c) != countT.end() && window[c] == countT[c]) {
-                have++;
-            }
-
-            while (have == need) {
-                if ((r - l + 1) < resLen) {
-                    resStart = l;
-                    resEnd = r;
-                    resLen = r - l + 1;
-                }
-                window[s[l]]--;
-
-                if (countT.find(s[l]) != countT.end() && window[s[l]] < countT[s[l]]) {
-                    have--;
-                }
-                l++;
-            }
-        }
-
-//         return (resLen != std::numeric_limits<int>::max()) ? s.substr(resStart, resEnd - resStart + 1) : "";
-         if (resLen != INT_MAX) {
-            return s.substr(resStart, resLen);
-        }
-        return "";
-    }
+  string minWindow(string s, string t) {
+  if(t.empty()) return "";
+      unordered_map<char, int>countT, window;
+      for(auto c: t){
+          countT[c]++;
+      }
+      int have=0, need=countT.size();
+      int resStart=0, resLen=INT_MAX, l=0;
+      for(int r=0; r<s.size(); r++){
+          window[s[r]]++;
+          if(countT.find(s[r])!=countT.end() && countT[s[r]]==window[s[r]]){
+              have++;
+          }
+          while(have==need){
+              if(r-l+1<resLen){
+                  resStart=l;
+                  resLen=r-l+1;
+              }
+             
+              window[s[l]]--;
+              if(countT.find(s[l])!=countT.end() && window[s[l]]<countT[s[l]]){
+                  have--;
+              }
+             l++;
+          }
+      }
+      if(resLen!=INT_MAX) return s.substr(resStart, resLen);
+      return "";
+  }
 };
-
-
-
-
-
-
-
