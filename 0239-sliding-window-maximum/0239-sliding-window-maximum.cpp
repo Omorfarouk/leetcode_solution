d62fcc55320bf374fc359c2296ea5ac08e1c37b3
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2023-08-11 00:29:21 +0600

    Time: 179 ms (99.41%), Space: 134.7 MB (65.96%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        deque<int>dq;
+        for(int i=0; i<k; i++){
+            while(!dq.empty() && nums[i]>dq.back()){
+                dq.pop_back();
+            }
+                dq.push_back(nums[i]);
+        }
+        res.push_back(dq.front());
+        
+        for(int i=k; i<nums.size(); i++){
+            if(nums[i-k]==dq.front()){
+                dq.pop_front();
+            }
+           while(!dq.empty() && nums[i]>dq.back()){
+             dq.pop_back();
+            }
+                dq.push_back(nums[i]);
+            res.push_back(dq.front());
+        }
+        return res;
+    }
+};
