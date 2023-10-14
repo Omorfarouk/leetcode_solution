0002238cf5c442760aae592bec3d7370c61d9fda
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2023-10-14 12:37:59 +0600

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,39 +6,74 @@ using namespace std;
 
 class Solution {
   public:
-    // Function to detect cycle in a directed graph.
-    
-    bool cycle(int s, vector<bool>&visited, vector<bool>&currVisited, vector<int> adj[]){
-        visited[s] = true;
-        currVisited[s] = true;
-        
-        for(auto x: adj[s]){
-            if(!visited[x]){
-                if(cycle(x, visited, currVisited, adj))
-                    return true;
-            }
-            else if(visited[x] == true && currVisited[x] == true){
-                return true;
-            }
-        }
-        
-        currVisited[s] = false;
-        return false;
-    }
-    
+  
+  bool cycle(int s, vector<bool>&vis, vector<bool>&currVis, vector<int>adj[]){
+      vis[s]=true;
+      currVis[s]=true;
+      for(auto x : adj[s]){
+          if(!vis[x]){
+              if(cycle(x,vis,currVis, adj))
+                  return true;
+              
+          }
+              else if(vis[x]==1 && currVis[x]==1){
+                  return true;
+              }
+          }
+          currVis[s]=false;
+          return false;
+      
+  }
     bool isCyclic(int V, vector<int> adj[]) {
-        // code here
-        vector<bool>visited(V, false), currVisited(V, false);
-        for(int i=0; i<V; i++){
-            if(!visited[i]){
-                if(cycle(i, visited, currVisited, adj))
-                    return true;
-            }
+    vector<bool>vis(V, false), currVis(V, false);
+    for(int i=0; i<V; i++){
+       if (!vis[i]){
+          if(cycle(i,vis,currVis,adj)){
+              return true;
+          }
         }
-        
-        return false;
+    }
+    return false;
     }
 };
+
+
+
+// class Solution {
+//   public:
+//     // Function to detect cycle in a directed graph.
+    
+//     bool cycle(int s, vector<bool>&visited, vector<bool>&currVisited, vector<int> adj[]){
+//         visited[s] = true;
+//         currVisited[s] = true;
+        
+//         for(auto x: adj[s]){
+//             if(!visited[x]){
+//                 if(cycle(x, visited, currVisited, adj))
+//                     return true;
+//             }
+//             else if(visited[x] == true && currVisited[x] == true){
+//                 return true;
+//             }
+//         }
+        
+//         currVisited[s] = false;
+//         return false;
+//     }
+    
+//     bool isCyclic(int V, vector<int> adj[]) {
+//         // code here
+//         vector<bool>visited(V, false), currVisited(V, false);
+//         for(int i=0; i<V; i++){
+//             if(!visited[i]){
+//                 if(cycle(i, visited, currVisited, adj))
+//                     return true;
+//             }
+//         }
+        
+//         return false;
+//     }
+// };
 
 //{ Driver Code Starts.
 
