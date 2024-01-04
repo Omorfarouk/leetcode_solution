2ab560cb092bc15a2d2473a7f6805e27040954c4
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2024-01-04 21:00:01 +0600

    Time: 4 ms (14.23%), Space: 11 MB (10.31%) - LeetHub

--- 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -8,13 +8,22 @@
 class Solution {
 public:
     int dp[1000][1000];
-	
+    
+	int res(int maxCom, int m, int n){
+         int ans = max(m-maxCom ,n-maxCom);
+        if(ans<=0){
+            return -1;
+        }
+        
+        return max(m,n);
+    }
+    
     int findLUSlength(string a, string b) {
         int m = a.size();
         int n = b.size();
         
-        for(int i=0; i<m+1; i++){
-            for(int j=0; j<n+1; j++){
+        for(int i=0; i<=m; i++){
+            for(int j=0; j<=n; j++){
                 if(i==0 or j==0) dp[i][j]=0;
                 else if(a[i-1]==b[j-1]){
                     dp[i][j] = dp[i-1][j-1]+1;
@@ -25,13 +34,11 @@ public:
             }
         }
         
+        
+       
         int maxCom = dp[m][n];
-        
-        int ans = max(m-maxCom ,n-maxCom);
-        if(ans<=0){
-            return -1;
-        }
-        
-        return max(m,n);
+       int ans=  res(maxCom,m,n);
+       
+        return ans;
     }
 };
