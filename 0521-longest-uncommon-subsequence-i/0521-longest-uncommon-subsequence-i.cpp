6f66b829aaac7c414a768a1bbba5ea0cef1530e7
Omor Farouk <88830457+Omorfarouk@users.noreply.github.com>
2024-01-04 20:02:33 +0600

    Time: 4 ms (14.23%), Space: 10.8 MB (10.31%) - LeetHub

---
+++ 0521-longest-uncommon-subsequence-i/0521-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+    int dp[1000][1000];
+	
+    int findLUSlength(string a, string b) {
+        int m = a.size();
+        int n = b.size();
+        
+        for(int i=0; i<m+1; i++){
+            for(int j=0; j<n+1; j++){
+                if(i==0 or j==0) dp[i][j]=0;
+                else if(a[i-1]==b[i-1]){
+                    dp[i][j] = dp[i-1][j-1]+1;
+                }
+                else{
+                    dp[i][j]= max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        
+        int sum = dp[m][n];
+        
+        int ans = max(m-sum ,n-sum);
+        if(ans<=0){
+            return -1;
+        }
+        
+        return max(m,n);
+    }
+};
